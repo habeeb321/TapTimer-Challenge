290e14d83a06cbbb2be8fd43c9545a5fa17e4db4
habeebu <habeebu@hello24.in>
2024-02-09 08:05:11 +0530

    Implemented seconds and random number widgets

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:tap_timer_challenge/views/home_screen/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Tap Timer Challenge',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            RandomAndSecondsWidget(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/home_screen/widgets/widgets.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class RandomAndSecondsWidget extends StatelessWidget {
+  const RandomAndSecondsWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          height: 110,
+          width: Get.width * 0.4,
+          decoration: BoxDecoration(
+            border: Border.all(color: const Color(0xff6F91C1)),
+            borderRadius: BorderRadius.circular(10),
+            color: const Color(0xffDAE8FC),
+          ),
+          child: const Column(
+            children: [
+              SizedBox(height: 40),
+              Text(
+                "Current Second",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Divider(
+                color: Color(0xff6F91C1),
+                thickness: 1,
+              ),
+              Text("37"),
+            ],
+          ),
+        ),
+        Container(
+          height: 110,
+          width: Get.width * 0.4,
+          decoration: BoxDecoration(
+            border: Border.all(color: const Color(0xffAC8EB8)),
+            borderRadius: BorderRadius.circular(10),
+            color: const Color(0xffE1D5E7),
+          ),
+          child: const Column(
+            children: [
+              SizedBox(height: 40),
+              Text(
+                "Random Number",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Divider(
+                color: Color(0xffAC8EB8),
+                thickness: 1,
+              ),
+              Text("10"),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
